8f905687825da93076f4418efd3215803df067e8
Ben Syverson <ben@bensyverson.com>
2014-07-02 01:46:08 -0500

    Initial commit

---
+++ ShowOtherMasters-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <GlyphsCore/GSLayer.h>
+#import <GlyphsCore/GSGlyph.h>
+#import <GlyphsCore/GlyphsReporterProtocol.h>
+#import <GlyphsCore/GSGlyphViewControllerProtocol.h>
+#import <GlyphsCore/MGOrderedDictionary.h>
---
+++ ShowOtherMasters/ShowOtherMasters.swift
@@ -0,0 +1,71 @@
+//
+//  ShowOtherMasters.swift
+//  ShowOtherMasters
+//
+//  Created by Ben Syverson on 7/1/14.
+//  Copyright (c) 2014 Ben Syverson. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+
+class ShowOtherMasters : NSObject, GlyphsReporter {
+	init() {
+		
+	}
+	
+	var _controller : NSViewController?
+	
+	func drawBackgroundForLayer(layer: GSLayer) {
+		
+	}
+	
+	func interfaceVersion() -> Int
+	{
+		return 1
+	}
+	
+	func title() -> String!
+	{
+		if let aString = NSBundle(forClass: ShowOtherMasters.self).localizedStringForKey("OtherMasters", value: "", table: nil) {
+			return aString
+		}
+		return "Other"
+	}
+	
+	func keyEquivalent() -> String!
+	{
+		return "`"
+	}
+	
+	func modifierMask() -> CUnsignedInt {
+		return NSCommandKeyMask
+	}
+	
+	func setController(Controller: NSViewController!)
+	{
+		_controller = Controller
+	}
+	
+	func drawBackgroundForLayer(Layer: GSLayer!)
+	{
+		
+	}
+	
+	func drawForegroundForLayer(Layer: GSLayer!)
+	{
+		let fadedColor = NSColor(white: 0.0, alpha: 0.2)
+		fadedColor.set()
+		
+		if let allLayers = Layer.parent?.layers?.allValues() {
+			for aLayer : AnyObject in allLayers {
+				if let myLayer = aLayer as? GSLayer {
+					myLayer.bezierPath.fill()
+				}
+			}
+		}
+		
+	}
+}
+
