33c2675059f6e225c87da99b6d7a67aef263d4de
Ben Syverson <ben@bensyverson.com>
2014-07-02 01:33:04 -0500

    Initial Commit

---
+++ ShowOtherMasters/ShowOtherMasters.h
@@ -0,0 +1,13 @@
+//
+//  ShowOtherMasters.h
+//  ShowOtherMasters
+//
+//  Created by Ben Syverson on 7/2/14.
+//  Copyright (c) 2014 Ben Syverson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ShowOtherMasters : NSObject {}
+
+@end
